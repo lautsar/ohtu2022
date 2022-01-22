ed793c8f861ff1b491c849d6f2230c412a488e73
Sari Lautanala <sari.lautanala@helsinki.fi>
2022-01-22 13:50:47 +0200

    Viikko 2 ekat tehtävät

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,22 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+
+        parsed = toml.loads(content)
+
+        name = parsed["tool"]["poetry"]["name"]
+        description = parsed["tool"]["poetry"]["description"]
+        dependencies = list(parsed["tool"]["poetry"]["dependencies"])
+        dev_dependencies = list(parsed["tool"]["poetry"]["dev-dependencies"])
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(name, description, dependencies, dev_dependencies)
